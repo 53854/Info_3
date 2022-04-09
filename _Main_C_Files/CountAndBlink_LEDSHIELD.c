328862d5d7e7845e815243832e97f3a8dba0aac9
Seb <53854k@gmail.com>
2022-04-09 17:46:52 +0200

    🤣

---
+++ _Main_C_Files/CountAndBlink_LEDSHIELD.c
@@ -0,0 +1,63 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#define F_CPU 16E6			// or F_CPU 16000000
+
+#define LED_ON PORTC |= (1<<1)
+#define LED_OFF PORTC &= ~(1<<1)
+
+#define LED1GN_TOGGLE PINC |= (1<<1)
+#define LED2GN_TOGGLE PINC |= (1<<2)
+#define LED3GN_TOGGLE PINC |= (1<<3)
+#define LED4GN_TOGGLE PINC |= (1<<4)
+
+#define BUTTON_ONE_PRESS (!(PIND & (1<<1)))
+#define BUTTON_TWO_PRESS (!(PINB & (1<<1)))
+
+unsigned blinkTimes = 0;
+
+void init(void){
+	// LED
+	DDRB |= (1<<1);		// Configure PB5 as Output
+	
+	// Buttons
+	DDRB &= ~(1<<1);	//Configure PB1 as Input
+	PORTB |= 1<<1;		//Enable Internal Pull-Up at PB1
+	
+	DDRD &= ~(1<<1);	//Configure PD1 as Input
+	PORTD |= 1<<1;		//Enable Internal Pull-Up at PD1
+}
+
+void blink(){
+	for(unsigned i = 0; i <= blinkTimes; i++){
+		LED_ON;
+		_delay_ms(5000);
+		LED_OFF;
+		_delay_ms(5000);
+	}
+}
+
+// void display_number_bin
+	// blinktimes to bin
+	// toggle LEDs
+	// Delay
+	// toggle LED
+
+int main(void){
+	init();
+	
+	while (1){
+		if(BUTTON_ONE_PRESS){
+			blink(blinkTimes ++);
+		} else if (BUTTON_TWO_PRESS){
+			blinkTimes = 0;	
+		}
+		
+		// if BUTTON_ONE_PRESS
+			// display_number_bin 16BitBinary
+		
+		// if (blinkTimes >= 16 || BUTTON_TWO_PRESS) 
+				// blinkTimes = 0
+				
+		
+	}
+}
--- _Main_C_Files/CountButtonAndBlink.c
+++ _Main_C_Files/CountButtonAndBlink.c
@@ -1,30 +1,39 @@
 #include <avr/io.h>
+#include <util/delay.h>
+#define F_CPU 16E6			// or F_CPU 16000000
 
 #define LED_ON PORTB |= (1<<5)
 #define LED_OFF PORTB &= ~(1<<5)
 #define LED_TOGGLE PINB |= (1<<5)
+
 #define BUTTON_PRESS (!(PINB & (1<<7)))
+
+unsigned buttonPresses = 0;
 
 void init(void){
 	// LED
-	DDRB |= (1<<5);// Configure PB5 as Output
-	LED_ON;
+	DDRB |= (1<<5);		// Configure PB5 as Output
+	
 	// Button
-	DDRB &= ~(1<<7);//Configure PB1 as Input
-	PORTB |= 1<<7;//Enable Internal Pull-Up at PB1
-	asm("nop");
+	DDRB &= ~(1<<7);	//Configure PB1 as Input
+	PORTB |= 1<<7;		//Enable Internal Pull-Up at PB1
 }
 
-void check_button(void){
-	if(BUTTON_PRESS)
-	LED_ON;
-	else
-	LED_OFF;
+void blink(){
+	for(unsigned i = 0; i <= buttonPresses; i++){
+		LED_ON;
+		_delay_ms(5000);
+		LED_OFF;
+		_delay_ms(5000);
+	}
 }
 
 int main(void){
 	init();
+	
 	while (1){
-		check_button();
+		if(BUTTON_PRESS){
+			blink(buttonPresses ++);
+		}
 	}
 }
