326a0b58c599e4aad40735d9482463a79b07f201
Seb <53854k@gmail.com>
2022-04-12 15:00:25 +0200

    😂😂😂😂😂😂😂😂 post lab fix

--- 09_Interrups_WITHSHIELD/09_Interrups_WITHSHIELD/main.c
+++ 09_Interrups_WITHSHIELD/09_Interrups_WITHSHIELD/main.c
@@ -3,12 +3,12 @@
 
 #define GREEN_ON {PORTC |= 0b00111110; PORTD |= 0b00110000;}
 #define GREEN_OFF {PORTC &= ~0b00111110; PORTD &= ~0b00110000;}
-#define GREEN_TOGGLE {PINC |= 0b00111110; PIND |= 0b00110000;}
+#define GREEN_TOGGLE {PORTC ^= 0b00111110; PORTD ^= 0b00110000;}
 
 
-#define RED_ON PORTD	|= 0b11000001
-#define RED_OFF PORTD	&= ~0b11000001
-#define RED_TOGGLE PIND |= 0b11000001
+#define RED_ON {PORTD |= 0b11000000; PORTB |= (1<<0);}
+#define RED_OFF {PORTD &= ~0b11000000; PORTB &= ~(1<<0);}
+#define RED_TOGGLE {PORTD ^= 0b11000000; PORTB ^= (1<<0);}
 
 #define BUTTON_ONE_PRESS (!(PIND & (1<<1))) // -> PCINT17
 #define BUTTON_TWO_PRESS (!(PINB & (1<<1))) // -> PCINT1
@@ -26,44 +26,35 @@ ISR(PCINT0_vect) // Triggers on PD changes
 
 void init(void){
 	
+	// DDR
+	DDRB |= 0b00000001;
+	DDRC |= 0b00111110;
+	DDRD |= 0b11110001;
+	
+	// Buttons
+	//DDRD &= ~(1<<1);	//Button 1 PD1
+	PORTD |= 1<<1;
+	
+	//DDRB &= ~(1<<1);	//Button 2 PB1
+	PORTB |= 1<<1;
+	
+	// turn on LEDS
+	GREEN_ON;
+	RED_ON;
 	
 	// Pin change interrupt btn1
 	sei();					// enable global interrupt
 	
-	PCICR |= (1<<PCIE2);	// enable PB
-	
 	PCICR |= (1<<PCIE2);	// enable PD PC interrupt
-	PCICR |= (1<<PCIE0);	// enable PB PC interrup
+	PCICR |= (1<<PCIE0);	// enable PB PC interrupt
 	
 	PCMSK0 |= (1<<1);	// mask out PB1 interrupt
 	PCMSK2 |= (1<<1);	// mask out PD1 interrupt
-	
-	// Buttons
-	DDRD &= ~(1<<1);	//Button 1 PD1
-	PORTD |= 1<<1;
-	
-	DDRB &= ~(1<<1);	//Button 2 PB1
-	PORTB |= 1<<1;
-	
-	// LEDS
-	DDRC |= 0b00111110;
-	DDRD |= 0b11110001;
-
-	// turn on green led
-	GREEN_ON;
-}
-
-void check_buttons(void){
-	if(BUTTON_ONE_PRESS)	{GREEN_ON;}
-	else					{GREEN_OFF;}
-	
-	if(BUTTON_TWO_PRESS)	{RED_ON;}
-	else					{RED_OFF;}
 }
 
 int main(void){
 	init();
 	while (1){
-		//check_buttons();
+		
 	}
 }
