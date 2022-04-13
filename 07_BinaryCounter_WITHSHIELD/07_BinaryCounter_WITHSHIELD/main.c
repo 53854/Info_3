76f15c76e184a9ee429c9df7a0bbc9c9aa776932
Seb <53854k@gmail.com>
2022-04-13 20:35:09 +0200

    cleanup
    thx nico

--- 07_BinaryCounter_WITHSHIELD/07_BinaryCounter_WITHSHIELD/main.c
+++ 07_BinaryCounter_WITHSHIELD/07_BinaryCounter_WITHSHIELD/main.c
@@ -1,43 +1,25 @@
+#define F_CPU 16E6			// or F_CPU 16000000
 #include <avr/io.h>
 #include <util/delay.h>
-
-
-#define F_CPU 16E6			// or F_CPU 16000000
 
 #define BUTTON_ONE_PRESS (!(PIND & (1<<1)))
 #define BUTTON_TWO_PRESS (!(PINB & (1<<1)))
 
-unsigned counter = 0;
-
+unsigned counter;
 
 void init(void){
+	// counter
+	counter = 0;
+	
 	// LED
-	DDRC |= 0b00011110;		// Configure PB5 as Output
-	PORTC &= ~0b00011110;
+	DDRC |= 0b00011110;		// Configure PC 1 - 4 as Output
 	
 	// Buttons
+	DDRB &= ~(1<<1);	//Configure PB1 as Input
 	PORTB |= 1<<1;		//Enable Internal Pull-Up at PB1
 	
 	DDRD &= ~(1<<1);	//Configure PD1 as Input
 	PORTD |= 1<<1;		//Enable Internal Pull-Up at PD1
-}
-
-void displayBinaryCount(unsigned c){
-
-	PORTC |= (1 << c); 
-	
-
-	/*int i = 1;
-	int n = counter;
-	while (n > 0) {
-		
-		if((n%2) == 1)	PORTC |= (1<<i);
-		else		PORTC &= ~(1<<i);
-	
-		n = n / 2;
-		i++;
-	}*/
-
 }
 
 
@@ -47,12 +29,13 @@ int main(void){
 	while (1){
  		if(BUTTON_ONE_PRESS){
 			counter ++;
-			PORTC &= ~0b11111111;
-			PORTC |= (1 << (counter%16)); 
-		} else if (BUTTON_TWO_PRESS){
+			counter %= 16;
+			PORTC = (counter << 1);
+			while(BUTTON_ONE_PRESS){}
+		} 
+		if (BUTTON_TWO_PRESS){
 			counter = 0;
+			PORTC = (counter << 1);
 		}
-		
-
 	}
 }
